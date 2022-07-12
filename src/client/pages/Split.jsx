3cba18808a5328e0fb32dceb7be7852989bba9c3
eyeandko <izuberi776@gmail.com>
2022-07-13 02:26:41 +0300

    more styling

--- src/client/pages/Split.jsx
+++ src/client/pages/Split.jsx
@@ -4,14 +4,19 @@ import React from "react";
 const Split = () => {
   return (
     <div>
-      <div className="login-container">
-      <form className="login-form">
-      <img src={"/assets/images/pie.png"}></img>
+      <div className="split-container">
+      <form className="split-form">
+      <div className="titleSplit">
       <h1> Split a Bill </h1>
-      <div>Expense Title: <input/></div>
-      <div>Total: $<input/></div>
-      <div>Number of Friends: <input/></div>
-      <button className="login-form-btn" type="submit">Split</button>
+      </div>
+      <img className="moneygif" src={"/assets/images/money.gif"}></img>
+      <div className="userInput">
+      <div className="expenseTitle">Expense Title <input className="titleInput"/></div>
+      <div className="total">Total $ <input className="totalInput"/></div>
+      <div className="splitNum">Split between <input className="splitInput"/> people</div>
+      <button className="split-btn" type="submit">Split</button>
+      <div className="calculation"><u>$50/person</u></div>
+      </div>
       </form>
       </div>
     </div>
