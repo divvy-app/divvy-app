d3ec08cfc3f0e4d2ae39a28ea37aeadf6568f09a
Terry Choi <yoonseok228@gmail.com>
2022-07-12 19:36:49 -0400

    Login button for oauth added

--- src/client/pages/Login.jsx
+++ src/client/pages/Login.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 // includes JSX
-const Login = () => (
-  <div>
-    <div>This is the login page</div>
-    <a
-      href="https://github.com/login/oauth/authorize?client_id=4d828c754c60a2276cbe"
-      class="btn btn-danger"
-    >
-      <span class="fa fa-github"></span> Github Login
-    </a>
-  </div>
-);
+const Login = () => {
+  const goGithub = () => {
+    window.location.href =
+      "https://github.com/login/oauth/authorize?client_id=4d828c754c60a2276cbe";
+  };
+
+  return (
+    <div>
+      <div>This is the login page</div>
+      <button id="githubBtn" onClick={goGithub}>
+        Login with GitHub
+      </button>
+    </div>
+  );
+};
 
 export default Login;
