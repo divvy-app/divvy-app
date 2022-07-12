f85b233d26228d47c7cc547556144bf9e98c1bb4
Parkreiner <throwawayclover@gmail.com>
2022-07-12 01:59:11 -0400

    misc: Add example file for React hooks

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,17 @@
+/**
+ * @file Defines the top-level app component for the Divvy app. Project is
+ * currently configured to accept a mix of .jsx and .tsx files.
+ */
+
 import React from "react";
 import "./style.css";
+import TSTest from "./TSTest";
 
 // includes JSX
 const App = () => (
   <div>
     <h1>Testing</h1>
+    <TSTest name="Pal" />
   </div>
 );
 
