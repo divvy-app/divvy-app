4e30ebc8d6412c34043a59169c068ec3b328484d
Chris I <chrisxesq@gmail.com>
2022-07-12 20:58:50 -0400

    landing page button links to login page

--- src/client/pages/Landing.jsx
+++ src/client/pages/Landing.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useNavigate } from 'react-router';
 
 // includes JSX
-const Landing = () => (
-  <div>
-    <button> Let's get started! </button>
-  </div>
-);
+const Landing = () => {
+  const navigate = useNavigate();
+
+  return (<div>
+    <button onClick={()=>{navigate('/login')}} > Let's get started! </button>
+  </div>)
+};
 
 export default Landing;
