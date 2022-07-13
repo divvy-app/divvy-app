dc5228eec20c4f2a8bbe493ae78154c3f5706f7a
eyeandko <izuberi776@gmail.com>
2022-07-13 03:34:35 +0300

    github button

--- src/client/pages/Login.jsx
+++ src/client/pages/Login.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 // includes JSX
 const Login = () => {
+  const goGithub = () => {
+    window.location.href =
+      "https://github.com/login/oauth/authorize?client_id=4d828c754c60a2276cbe";
+  };
   return (
   <div>
     <div className="login-container">
     <form className="login-form">
     <h1> Welcome back </h1>
+    <button id="githubBtn" onClick={goGithub}><img src={"/assets/images/github.png"}></img>Login with GitHub</button>
     <div className="input-login">
     <label htmlFor="email">Email </label>
     <div><input id="email" type="text" name="email" className="email-label" placeholder="Enter your email"/></div>
@@ -19,26 +24,11 @@ const Login = () => {
     <div className="form-input-btn">
       <button className="login-form-btn" type="submit">Login</button>
     </div>
-    <div className="github">or Sign in with Github</div>
     <div className="signup">Don't have an account? <a href="http://localhost:8080/signup">Sign Up Here</a></div>
     </form>
     </div>
   </div>
   )
-
-  const goGithub = () => {
-    window.location.href =
-      "https://github.com/login/oauth/authorize?client_id=4d828c754c60a2276cbe";
-  };
-
-  return (
-    <div>
-      <div>This is the login page</div>
-      <button id="githubBtn" onClick={goGithub}>
-        Login with GitHub
-      </button>
-    </div>
-  );
 };
 
 export default Login;
